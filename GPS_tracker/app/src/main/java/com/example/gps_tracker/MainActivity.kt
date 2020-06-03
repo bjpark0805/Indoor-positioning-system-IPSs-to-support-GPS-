b000703c108bf56876f8233f3e8b43646ecb5b91
Lumy Kelvin <30566564+KLumy@users.noreply.github.com>
2020-06-03 23:03:45 +0900

    shit

--- GPS_tracker/app/src/main/java/com/example/gps_tracker/MainActivity.kt
+++ GPS_tracker/app/src/main/java/com/example/gps_tracker/MainActivity.kt
@@ -10,21 +10,22 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.Environment
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.concurrent.timer
 
 
@@ -35,8 +36,8 @@ const val PERMISSIONS_REQUEST_CODE = 100
 
 class MainActivity : AppCompatActivity() {
     var REQUIRED_PERMISSIONS = arrayOf<String>(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
     )
     private var time = 0
     private var isRunning = false
@@ -44,27 +45,28 @@ class MainActivity : AppCompatActivity() {
     private var lap = 1
     private var lastTimeBackPressed: Long = -1500
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerView.Adapter<*>
+
+    private val coordinationList: ArrayList<String> = ArrayList<String>()
+
 //    val path = "/data/data/GPS_tracker/files/experiment/${current_time}.txt"
 
 
-
-
-    private fun start(file: File){
+    private fun start(file: File) {
         play_fab.setImageResource(R.drawable.ic_pause)
         val textView = TextView(this)
         val gpsTracker = GpsTracker(this@MainActivity)
-        timerTask = timer(period = 10000){
+        timerTask = timer(period = 1000) {
             time++
 
             val latitude = gpsTracker.getLatitude()
             val longitude = gpsTracker.getLongitude()
 
-            val coordinate = "Location: \nlatitude $latitude\nlongitude $longitude"
+            val coordinate = "Location: latitude $latitude, longitude $longitude"
             textView.text = coordinate
 
-
-
-            try{
+            try {
                 val now = System.currentTimeMillis()
                 val date = Date(now)
                 val sdfNow = SimpleDateFormat("yy_MM_dd_HH_mm_ss")
@@ -73,40 +75,39 @@ class MainActivity : AppCompatActivity() {
                 buf.append("$currentTime : $coordinate")
                 buf.newLine()
                 buf.close()
-            }catch (e: FileNotFoundException){
+                recyclerView.
+
+            } catch (e: FileNotFoundException) {
                 e.printStackTrace()
-            }catch(e: IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
             lap++
 
-            runOnUiThread{
+            runOnUiThread {
                 Toast.makeText(this@MainActivity, coordinate, Toast.LENGTH_LONG).show()
-                if (textView.getParent() != null) {
-                    (textView.getParent() as ViewGroup).removeView(textView) // <- fix
-                }
-                labLayout.addView(textView, 0)
                 // file write??
             }
 
         }
     }
-    private fun pause(){
+
+    private fun pause() {
         play_fab.setImageResource(R.drawable.ic_stat_name)
         // Timer객체의 cancel메소드로 타이머 중지 가능
         timerTask?.cancel()
     }
 
-    private fun reset(){
+    private fun reset() {
         timerTask?.cancel()
 
-        if(isRunning)
+        if (isRunning)
             isRunning = false
 
         time = 0
         play_fab.setImageResource(R.drawable.ic_stat_name) // ic_stat_name == play (I misnamed the icon)
 //        lapLayout.removeAllViews()
-        lap=1
+        lap = 1
 
         // File write all the things in lapLayout
     }
@@ -121,14 +122,14 @@ class MainActivity : AppCompatActivity() {
         val filename = "/${today}.txt"
         val FileDirectory = getExternalFilesDir(null)
         val file = File(FileDirectory, filename)
-        if (file.exists()){
+        if (file.exists()) {
             Toast.makeText(this@MainActivity, filename, Toast.LENGTH_LONG).show()
 
         }
 
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting()
-        }else {
+        } else {
             checkRunTimePermission();
         }
 
@@ -137,33 +138,38 @@ class MainActivity : AppCompatActivity() {
         // ShowLocationButton is for the test.
         val showLocationButton = findViewById<Button>(R.id.button)
 
-        val clickListener = View.OnClickListener { view-> //view를 딱히 사용은 안해서 그냥 아무것도 안해놨는데 나중에 view가 필요하면 update
-            val gpsTracker = GpsTracker(this@MainActivity)
+        val clickListener =
+            View.OnClickListener { view -> //view를 딱히 사용은 안해서 그냥 아무것도 안해놨는데 나중에 view가 필요하면 update
+                val gpsTracker = GpsTracker(this@MainActivity)
 
-            val latitude = gpsTracker.getLatitude()
-            val longitude = gpsTracker.getLongitude()
+                val latitude = gpsTracker.getLatitude()
+                val longitude = gpsTracker.getLongitude()
 
 //            val address = getCurrentAddress(latitude, longitude)
 //            textviewaddress.text = address
-            val string = "Location: \nlatitude $latitude\nlongitude $longitude"
-            Toast.makeText(this@MainActivity, string, Toast.LENGTH_LONG).show()
-        }
+                val string = "Location: \nlatitude $latitude\nlongitude $longitude"
+                Toast.makeText(this@MainActivity, string, Toast.LENGTH_LONG).show()
+            }
 
         showLocationButton.setOnClickListener(clickListener)
 
-        play_fab.setOnClickListener{
-            when(isRunning){
-                false->start(file)
-                true->pause()
+        play_fab.setOnClickListener {
+            when (isRunning) {
+                false -> start(file)
+                true -> pause()
             }
-            isRunning =  !isRunning
+            isRunning = !isRunning
         }
 
-        clear.setOnClickListener{
+        clear.setOnClickListener {
             reset()
         }
 
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = MyAdapter(coordinationList)
+        recyclerView.adapter = adapter;
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -171,7 +177,7 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size){
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
             // If the requested code equals PERMISSIONS_REQUEST_CODE, and the number of received equals required_permission
             var check = true
             // Check all the permissions 모든 퍼미션을 허용했는지 체크합니다.
@@ -182,24 +188,39 @@ class MainActivity : AppCompatActivity() {
                     break;
                 }
             }
-            if(!check){
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
-                    || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+            if (!check) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[0]
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[1]
+                    )
+                ) {
 
-                    Toast.makeText(this@MainActivity, "Permission denied. Restart the Application.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission denied. Restart the Application.",
+                        Toast.LENGTH_LONG
+                    ).show();
                     finish();
 
 
-                }else {
+                } else {
 
-                    Toast.makeText(this@MainActivity,"Permission denied. You need to change your setting to get permission.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission denied. You need to change your setting to get permission.",
+                        Toast.LENGTH_LONG
+                    ).show();
 
                 }
             }
         }
     }
 
-    fun checkRunTimePermission(){
+    fun checkRunTimePermission() {
         //Runtime permission check
         // 1. Check location permission available
 
@@ -213,12 +234,19 @@ class MainActivity : AppCompatActivity() {
         )
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
             // This case, already has permission, nothing to do.
-        }
-        else{
+        } else {
             // Request permission to the user to run the app.
-            Toast.makeText(this@MainActivity, "Need a permission to GPS to run this application.", Toast.LENGTH_LONG).show();
+            Toast.makeText(
+                this@MainActivity,
+                "Need a permission to GPS to run this application.",
+                Toast.LENGTH_LONG
+            ).show();
             // Request result is received at onRequestPermissionResult.
-            ActivityCompat.requestPermissions(this@MainActivity, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                REQUIRED_PERMISSIONS,
+                PERMISSIONS_REQUEST_CODE
+            );
         }
     }
 
@@ -290,9 +318,9 @@ class MainActivity : AppCompatActivity() {
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
 
-    fun saveFile(inputStream: InputStream, filePath: String){
+    fun saveFile(inputStream: InputStream, filePath: String) {
         val saveFile = File(filePath)
-        saveFile.outputStream().use{fileOutput->
+        saveFile.outputStream().use { fileOutput ->
             inputStream.copyTo(fileOutput)
         }
     }
---
+++ GPS_tracker/app/src/main/java/com/example/gps_tracker/MyAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.gps_tracker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(var items: ArrayList<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        public var textView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items.get(position)
+    }
+
+    fun addData(data: String) {
+        this.items.add(data)
+        notifyDataSetChanged()
+    }
+
+}
